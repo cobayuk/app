a0ca6a88a2261fbe9fa3ac4d097de440f88c8fd7
cobayuk <48874869+cobayuk@users.noreply.github.com>
2020-01-05 20:45:10 +0700

    Add files via upload

---
+++ newsApi.js
@@ -0,0 +1,5 @@
+const apiKey = '72b23c6ecea04daf88dae940db76ce6d';
+const topHeadlinesUrl =
+  'https://newsapi.org/v2/top-headlines?country=us&apiKey=' + apiKey;
+
+export { apiKey, topHeadlinesUrl };
